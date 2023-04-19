ab005a392199558fdba17b607f9789df72ccc559
Devison Santos <devison_santos@yahoo.com.br>
2023-04-18 21:36:05 -0300

    Comandos de fluxo 4.1 ate 4.4

---
+++ 4-Controle-Fluxo/comando_continue.py
@@ -0,0 +1,5 @@
+for num in range(2, 10):
+    if num % 2 == 0:
+        print('Numero Par', num)
+        continue
+    print("Numero Impar", num)
---
+++ 4-Controle-Fluxo/comando_if.py
@@ -0,0 +1,10 @@
+x = int(input("Digite um numero intero: "))
+if x < 0:
+    x = 0
+    print("Numero negativo, alterado para zero")
+elif x == 0:
+    print("zero")
+elif x == 1 :
+    print("um")
+else:
+    print("mais")
